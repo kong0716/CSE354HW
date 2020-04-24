c15d2a6341aa0180ba929ec9ad3f49dc609eac1f
Darren Kong <darren.kong@stonybrook.edu>
2020-04-24 14:42:06 -0400

    Fixed import issues and finished loading of csv

--- a3_Kong_110770716.py
+++ a3_Kong_110770716.py
@@ -5,8 +5,19 @@ from sklearn.linear_model import Ridge #just like L2 regularized logistic regres
 from sklearn.decomposition import PCA #just like L2 regularized logistic regression
 import scipy.stats as ss #for distributions
 from gensim.models import Word2Vec
+from happiestfuntokenizing.happiestfuntokenizing import Tokenizer
 import sys
 from collections import Counter, defaultdict
+
+def preparecsv(filename):
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        id_dict = dict()
+        for row in reader:
+            unique_id = row['id']
+            id_dict.update({unique_id : [row['rating'], row['vote'], row['asin'], row['user_id'], row['reviewText']]})
+        #print(id_dict)
+        return id_dict
 
 def main(argv):
     if len(argv) != 2:
@@ -14,6 +25,9 @@ def main(argv):
     else:
         print(argv[0])
         print(argv[1])
-
+        train_csv = preparecsv(argv[0])
+        test_csv = preparecsv(argv[1])
+        return train_csv, test_csv
+        
 if __name__== '__main__':
     main(sys.argv[1:])
