df353554b42d3e63d8901dbe0c912e5f83b4696d
Darren Kong <darren.kong@stonybrook.edu>
2020-04-24 21:31:20 -0400

    Started work on model

--- a3_Kong_110770716.py
+++ a3_Kong_110770716.py
@@ -1,6 +1,7 @@
 #Darren Kong
 #CSE354 HW 3
 import  re, numpy as np, sklearn, scipy.stats, pandas, csv, gensim
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Ridge #just like L2 regularized logistic regression
 from sklearn.decomposition import PCA #just like L2 regularized logistic regression
 import scipy.stats as ss #for distributions
@@ -24,7 +25,6 @@ def preparecsv(filename):
                 id_dict.update({unique_id : [row['rating'], row['vote'], row['asin'], row['user_id'], row['reviewText']]})
         #print(id_dict)
         return id_dict
-
 # Step 1.2 Tokenize the file.
 def tokenizeReviews(id, csv_dict):
     tokenizer = Tokenizer()
@@ -54,7 +54,6 @@ def extractMeanFeaturesVector(id, csv_dict, model):
     #print(wordVector)
     return wordVector
 # Step 1.5 Build a rating predictor using L2 *linear* regression (can use the SKLearn Ridge class) with word2vec features.
-
 def extractFeaturesVectors(csv_dict, model):
     idList = list(csv_dict.keys())
     # Paranoia
@@ -63,7 +62,6 @@ def extractFeaturesVectors(csv_dict, model):
     for id in idList:
         reviewVectors += [extractMeanFeaturesVector(id, csv_dict, model)]
     return reviewVectors
-
 def extractYVector(csv_dict):
     idList = list(csv_dict.keys())
     # Paranoia
@@ -75,6 +73,12 @@ def extractYVector(csv_dict):
     #print(len(yVector))
     return yVector
 
+def trainRater(features, ratings):
+    #inputs: features: feature vectors (i.e. X)
+    #        ratings: ratings value [0, 1, 2, 3, 4, 5] (i.e. y)
+    #output: model -- a trained sklearn.linear_model.Ridge object
+    return
+
 def main(argv):
     if len(argv) != 2:
         print("Needs a train and test file")
@@ -82,14 +86,21 @@ def main(argv):
         train_csv = preparecsv(argv[0])
         test_csv = preparecsv(argv[1])
         sentences = genWord2Vec(train_csv)
-        model = gensim.models.Word2Vec(sentences=sentences, size=128, min_count=1)
+        model = gensim.models.Word2Vec(sentences=sentences, 
+                                       size=128, 
+                                       min_count=1)
 
         vec_great = model.wv['great']
         print(model)
         print(vec_great)
         Xs = extractFeaturesVectors(train_csv, model)
-        Ys = extractYVector(train_csv)
-        
+        ys = extractYVector(train_csv)
+
+        X_train, X_test, y_train, y_test = train_test_split(np.array(Xs),
+                                                            np.array(ys),
+                                                            test_size=0.20)
+
+        rater = trainRater(X_train, y_train)
         return train_csv, test_csv  
         
 if __name__== '__main__':
